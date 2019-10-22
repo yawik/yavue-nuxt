5c9f9e3afd96af9b93d5b6c9e73fef572ed28c62
Carsten Bleek <bleek@cross-solution.de>
2019-10-22 20:35:18 +0200

    makes "npm run dev" acessible via public IP

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,10 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  server: {
+    port: 8000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
   mode: 'universal',
   /*
   ** Headers of the page
